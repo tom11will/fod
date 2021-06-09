83597b8cb0db6178cdeb7ef14cb4ebdcdfb73b27
tom11will <85643176+tom11will@users.noreply.github.com>
2021-06-09 14:32:48 -0400

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60c10655436263001e1ed79f",
+				"60c10655436263001e1ed7a1"
+			]
+		},
+		"60c10655436263001e1ed79f": {
+			"id": "60c10655436263001e1ed79f",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60c10655436263001e1ed7a1": {
+			"id": "60c10655436263001e1ed7a1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
